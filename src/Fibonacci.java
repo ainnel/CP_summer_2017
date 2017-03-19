6cbd1a8f579dfc4ccb93f7693b9a2cc975b8e249
ainnel <annalovestom@gmail.com>
2017-03-19 16:34:13 +0100

    Homework2

---
+++ src/Fibonacci.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Ania on 19.03.2017.
+ */
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int n = 10;        /* number of Fibonacci numbers */
+        int f1 = 0, f2 = 1;  /* fn , fn+1 */
+        for (int i = 0; i <= n; i++) {
+            System.out.println(f1);
+            f1 = f1 + f2;
+            f2 = f1 - f2;
+        }
+    }
+}
