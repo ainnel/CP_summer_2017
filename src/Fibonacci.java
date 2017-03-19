eed635c2f8a2944ced17348c0469c767a1020a17
ainnel <annalovestom@gmail.com>
2017-03-19 16:57:41 +0100

    Homework2

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,12 +4,12 @@
 public class Fibonacci {
 
     public static void main(String[] args) {
-        int n = 10;        /* number of Fibonacci numbers */
+        int n = 14;        /* number of Fibonacci numbers */
         int f1 = 0, f2 = 1;  /* fn , fn+1 */
         for (int i = 0; i <= n; i++) {
             System.out.println(f1);
-            f1 = f1 + f2;
-            f2 = f1 - f2;
+            f2 = f1 + f2;
+            f1 = f2 - f1;
         }
     }
 }
