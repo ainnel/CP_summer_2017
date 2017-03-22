795561c534d994727686c6ccbabeb16b0c41b3d0
ainnel <annalovestom@gmail.com>
2017-03-22 18:33:08 +0100

    HelloWorld

---
+++ src/TextFileRead.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by Ania on 22.03.2017.
+ */
+public class TextFileRead {
+
+    public static void main(String[] args) {
+
+
+        System.out.print("Please provide input: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        System.out.println(input);
+    }
+}
+
