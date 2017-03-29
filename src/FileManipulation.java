b46a2bd0b461382b4c95cf142d0967b2fa3cb250
ainnel <annalovestom@gmail.com>
2017-03-29 16:58:02 +0100

    HelloWorld

--- src/FileManipulation.java
+++ src/FileManipulation.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 /**
  * Created by Ania on 29.03.2017.
  */
@@ -10,8 +12,18 @@ public class FileManipulation {
         System.out.println(
                 System.getProperty("java.io.tmpdir"));
 
-        System.out.println(System.getenv("PATH"));
+        System.out.println(System.getenv("C:\\Project"));
 
         System.out.println(System.getProperty("user.dir"));
+
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        File tmpDirProject = new File(tmpDir);
+        File[] fileList = tmpDirProject.listFiles();
+        for (File f : fileList) {
+            System.out.println(f.length());
+            int sum = (int) f.length();
+            System.out.println(sum);
+
+        }
     }
 }
