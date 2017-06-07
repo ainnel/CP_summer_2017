f9ad4861be36f828d6af6f24032719cf4ff1087f
ainnel <annalovestom@gmail.com>
2017-06-07 12:27:44 +0100

    Threads

--- src/Homework5.java
+++ src/Homework5.java
@@ -1,33 +1,45 @@
-import java.io.File;
+
+import java.io.FileReader;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.io.BufferedReader;
+
 /**
  * Created by Ania on 09.05.2017.
  */
 public class Homework5 {
-    public static void main(String[] args) throws FileNotFoundException, java.io.IOException {
-        File myFile = new File("C:\\Workingfromhome.docx");
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(myFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        int i = 0;
-        while (scanner.hasNext()) {
-            Map<String, File> shapeMap = new HashMap<>();
-            File word; word = shapeMap.put("word", new File(String.valueOf(5)));
 
-            System.out.println("word: " + shapeMap.get("word"));
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Map map = new HashMap();
 
-            System.out.println();
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Workingfromhome.docx"))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-            for (String s : shapeMap.keySet()) {
-                System.out.println(s + " = " + shapeMap.get(s));
+            while (line != null) {
+                String[] words = line.split(" ");
+                for (int i = 0; i < words.length; i++) {
+                    if (map.get(words[i]) == null) {
+                        map.put(words[i], 1);
+                    } else {
+                        int newValue = Integer.valueOf(String.valueOf(map.get(words[i])));
+                        newValue++;
+                        map.put(words[i], newValue);
+                    }
+                }
+                sb.append(System.lineSeparator());
+                line = br.readLine();
             }
+        }
+        Map<String, String> sorted = new TreeMap<String, String>(map);
+        for (Object key : sorted.keySet())
 
+        {
+            System.out.println("Word: " + key + "\tCounts: " + map.get(key));
         }
     }
 }
+
